e91bd4e28a332f8ac011c98bf6345eae343acafb
sazzledazzle <sarahclements@hotmail.co.uk>
2021-02-24 14:52:33 +0000

    working on layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
-module.exports = function(eleventyConfig) {
+const shortcodes = require('./utils/shortcodes.js');
+const iconsprite = require('./utils/iconsprite.js');
+
+module.exports = function(config) {
     // So 11ty can use the files inside these folders
     //eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPassthroughCopy("src/js");
+    config.addPassthroughCopy("src/images");
+    config.addPassthroughCopy("src/js");
+
+        // Shortcodes
+    Object.keys(shortcodes).forEach((shortcodeName) => {
+        config.addShortcode(shortcodeName, shortcodes[shortcodeName])
+    })
+
+    // Icon Sprite
+    config.addNunjucksAsyncShortcode('iconsprite', iconsprite)
 
     return {
         // Change output directory so can host on Github Pages. In Github change the 'source' to /docs
---
+++ utils/shortcodes.js
@@ -0,0 +1,10 @@
+// Copied code from Max Boeck's 'eleventastic' repository
+
+module.exports = {
+    icon: function (name) {
+        return `<svg class="icon icon--${name}" role="img" aria-hidden="true" width="24" height="24">
+                    <use xlink:href="#icon-${name}"></use>
+                </svg>`
+    }
+}
+
